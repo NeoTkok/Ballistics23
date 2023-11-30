1cfea99311d62d7d2e4c9b20e3251d1665b59ee0
Tkok <murdereruu@yandex.ru>
2023-11-30 19:43:49 +0300

    add struct

---
+++ src/Ballistics/time/time.cpp
@@ -0,0 +1,11 @@
+//
+// Created by neodima on 09.11.23.
+//
+
+#include "time.hpp"
+
+namespace Ballistics23::Time {
+
+//    Time<>::Time(double jdInt = 0, double jdPart = 0) noexcept;
+
+}
---
+++ tests/time/test_time.cpp
@@ -0,0 +1,10 @@
+//
+// Created by neodima on 26.10.23.
+//
+
+#include "Eigen/Dense"
+#include <gtest/gtest.h>
+
+TEST(AddTest, PositiveNumbers) {
+    EXPECT_EQ(4, 2+2); // Ожидаем, что 2 + 2 = 4
+}
