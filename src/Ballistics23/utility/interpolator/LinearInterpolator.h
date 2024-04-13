c2c90546cb0ec6e93eeee73d5bf7da72b0b6257f
Tkok <murdereruu@yandex.ru>
2024-04-13 22:18:31 +0300

    added interpolator

---
+++ src/Ballistics23/utility/interpolator/LinearInterpolator.h
@@ -0,0 +1,54 @@
+//
+// Created by neodima on 04.12.23.
+//
+
+#include "Ballistics23/exceptions/Exceptions.h"
+#include "Ballistics23/utility/types/BasicTypes.h"
+
+namespace Ballistics23::Utility {
+
+template <typename xType, typename yType> class Interpolator {
+
+public:
+  struct XY {
+    xType x;
+    yType y;
+  };
+
+private:
+  Containers::vector<XY> data_;
+
+public:
+  Interpolator() = default;
+
+  explicit Interpolator(const Containers::vector<XY> &xy) noexcept
+      : data_(xy){};
+
+  [[nodiscard]] scalar interpolateDUT(const scalar xPoint) const {
+
+    for (indexType i = 0; i < data_.size() - 1; ++i) {
+
+      if ((xPoint >= data_[i].x) && (xPoint < data_[i + 1].x)) {
+
+        const yType dutDiff = data_[i + 1].y - data_[i].y;
+        const yType leapAdd =
+            (std::abs(dutDiff) > static_cast<yType>(0.6))
+                ? -dutDiff / std::abs(dutDiff) * static_cast<yType>(1)
+                : static_cast<yType>(0);
+
+        const yType numerator = data_[i + 1].y - data_[i].y + leapAdd;
+        const xType denominator = data_[i + 1].x - data_[i].x;
+
+        return data_[i].y + numerator / denominator * (xPoint - data_[i].x);
+      }
+    }
+
+    if (xPoint == data_[data_.size() - 1].x) {
+      return data_[data_.size() - 1].y;
+    }
+
+    throw Exceptions::Exception("INTERPOLATOR ERROR: VALUE OUT OF BOUNDS");
+  }
+};
+
+} // namespace Ballistics23::Utility
