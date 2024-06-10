fbba0a0ced1a8853241a7c00edaec93b802a1880
Tkok <murdereruu@yandex.ru>
2024-06-10 17:27:25 +0300

    added acceleration

---
+++ src/Ballistics23/forces/AccelerationCalculator.h
@@ -0,0 +1,130 @@
+//
+// Created by neodima on 20.02.24.
+//
+
+#pragma once
+
+#include "Ballistics23/time/Time.h"
+#include "Ballistics23/utility/types/BasicTypes.h"
+
+namespace Ballistics23::Force {
+
+template <typename TimeConverter, typename FrameConverter,
+          typename EphemerisCalculator, typename EarthGravity,
+          typename... OtherForces>
+class AccelerationCalculator {
+  using TimeTT = TimeModule::Time<TimeModule::TimeScale::TT_SCALE>;
+
+public:
+  struct allSatParams : OtherForces::SatParams... {};
+
+private:
+  TimeConverter timeConverter_;
+  FrameConverter frameConverter_;
+  EphemerisCalculator ephemerisCalculator_;
+
+  struct InputParams {
+    Vector3d moonPositionGCRS;
+    Vector3d jupiterPositionGCRS;
+    Vector3d sunPositionGCRS;
+    Vector3d sunVelocityGCRS;
+
+    double moonGravParameter;
+    double jupiterGravParameter;
+    double sunGravParameter;
+  };
+
+  [[nodiscard]] InputParams calcInputParam(
+      const TimeTT timeTT,
+      const Quaternion<double> &quaternionGCRStoITRS) const noexcept {
+
+    const TimeModule::Time<TimeModule::TimeScale::TDB_SCALE> timeTDB =
+        timeConverter_.convertTT_TDB(timeTT);
+
+    const auto stdToVector3d = [](const Containers::vector<double> &vector3) {
+      return Vector3d{vector3[0], vector3[1], vector3[2]};
+    };
+
+    const int centerBody = 3;
+    const Vector3d moonPositionGCRS = stdToVector3d(
+        ephemerisCalculator_.calculateBodyR(10, centerBody, timeTDB));
+
+    const Vector3d jupiterPositionGCRS = stdToVector3d(
+        ephemerisCalculator_.calculateBodyR(5, centerBody, timeTDB));
+
+    const Containers::vector<double> sunRVgcrs =
+        ephemerisCalculator_.calculateBodyRV(11, centerBody, timeTDB);
+
+    const Vector3d sunPositionGCRS = {sunRVgcrs[0], sunRVgcrs[1], sunRVgcrs[2]};
+    const Vector3d sunVelocityGCRS = {sunRVgcrs[3], sunRVgcrs[4], sunRVgcrs[5]};
+
+    const Vector3d moonPositionITRS =
+        quaternionGCRStoITRS._transformVector(moonPositionGCRS);
+    const Vector3d jupiterPositionITRS =
+        quaternionGCRStoITRS._transformVector(jupiterPositionGCRS);
+    const Vector3d sunPositionITRS =
+        quaternionGCRStoITRS._transformVector(sunPositionGCRS);
+    const Vector3d sunVelocityITRS =
+        quaternionGCRStoITRS._transformVector(sunVelocityGCRS);
+
+    const double muMoon = ephemerisCalculator_.calcGravParameter(10);
+    const double muJupiter = ephemerisCalculator_.calcGravParameter(5);
+    const double muSun = ephemerisCalculator_.calcGravParameter(11);
+
+    return {moonPositionITRS,
+            jupiterPositionITRS,
+            sunPositionITRS,
+            sunVelocityITRS,
+            muMoon,
+            muJupiter,
+            muSun};
+  }
+
+  template <unsigned int... Is>
+  [[nodiscard]] Vector3d
+  auxFunction(const std::tuple<OtherForces...> &otherForces,
+              const TimeTT timeTT, const Vector3d &positionGCRS,
+              const Vector3d &velocityGCRS, double mass,
+              const allSatParams &allSatParams, const InputParams &inputParams,
+              std::integer_sequence<unsigned int, Is...>) const noexcept {
+
+    return (std::get<Is>(otherForces)
+                .calcAcceleration(timeTT, positionGCRS, velocityGCRS, mass,
+                                  allSatParams, inputParams) +
+            ...);
+  }
+
+public:
+  AccelerationCalculator(const TimeConverter &timeConverter,
+                         const FrameConverter &frameConverter,
+                         const EphemerisCalculator &ephemerisCalculator,
+                         EarthGravity &,
+                         const std::tuple<OtherForces...> &) noexcept
+      : timeConverter_(timeConverter), frameConverter_(frameConverter),
+        ephemerisCalculator_(ephemerisCalculator){};
+
+  [[nodiscard]] Vector3d
+  calcAcceleration(const EarthGravity &earthGravity,
+                   const std::tuple<OtherForces...> &otherForces,
+                   const TimeTT timeTT, const Vector3d &positionITRS,
+                   const Vector3d &velocityITRS, double mass,
+                   const allSatParams &allSatParams) const noexcept {
+
+    const Quaternion<double> quaternionGCRStoITRS =
+        frameConverter_.quaternionGCRStoITRS(timeTT);
+
+    const InputParams inputParams =
+        calcInputParam(timeTT, quaternionGCRStoITRS);
+
+    const Vector3d earthGravAccelerationITRS =
+        earthGravity.calcAccelerationECEF(positionITRS);
+
+    const Vector3d otherForcesAcceleration = auxFunction(
+        otherForces, timeTT, positionITRS, velocityITRS, mass, allSatParams,
+        inputParams,
+        std::make_integer_sequence<unsigned int, sizeof...(OtherForces)>{});
+
+    return earthGravAccelerationITRS + otherForcesAcceleration;
+  }
+};
+} // namespace Ballistics23::Force
---
+++ tests/ballistics23/forces/test_AccelerationCalculator.cpp
@@ -0,0 +1,147 @@
+//
+// Created by neodima on 20.02.24.
+//
+
+#include <gtest/gtest.h>
+
+#include "utility/parser/BulletinParser.h"
+
+#include "ephemeris/EphemerisCalculator.h"
+#include "forces/AccelerationCalculator.h"
+#include "forces/Gravity/CelestialGravity.h"
+#include "forces/Gravity/EarthGravity.h"
+#include "frame/FrameConverter.h"
+#include "frame/PolarMotionContainer.h"
+#include "time/DutCorrection.h"
+#include "time/TimeConverter.h"
+
+using namespace Ballistics23::Utility;
+using namespace Ballistics23::Containers;
+
+TEST(ACCELERATION, ALL) {
+
+  const auto begin = static_cast<Ballistics23::scalar>(0);
+  const auto end = static_cast<Ballistics23::scalar>(100000);
+
+  const std::string currentDir = getRootPath(__FILE__, 4);
+  const std::string csvPath = currentDir + "/data/frame/earth_rotation.csv";
+
+  const Ballistics23::indexType mjdColInd = 3;
+  const Ballistics23::indexType xpColInd = 4;
+  const Ballistics23::indexType ypColInd = 5;
+  const Ballistics23::indexType dutColInd = 6;
+
+  vector<double> mjdData = getColumn(csvPath, mjdColInd, mjdColInd, begin, end);
+  vector<double> xpData = getColumn(csvPath, xpColInd, mjdColInd, begin, end);
+  vector<double> ypData = getColumn(csvPath, ypColInd, mjdColInd, begin, end);
+  vector<double> dutData = getColumn(csvPath, dutColInd, mjdColInd, begin, end);
+
+  const Ballistics23::FrameModule::PolarMotionContainer polarMotionContainer(
+      mjdData, xpData, ypData);
+  const Ballistics23::TimeModule::DutContainer dutContainer(mjdData, dutData);
+
+  const Ballistics23::TimeModule::TimeConverter<
+      Ballistics23::TimeModule::DutContainer>
+      timeConverter(dutContainer);
+
+  const Ballistics23::FrameModule::FrameConverter<
+      Ballistics23::FrameModule::PolarMotionContainer,
+      Ballistics23::TimeModule::DutContainer>
+      frameConverter(polarMotionContainer, timeConverter);
+
+  const std::string ephemerisPath = currentDir + "/data/ephemeris/de405.bin";
+
+  const Ballistics23::Ephemeris::EphemerisCalculator ephemerisCalculator(
+      ephemerisPath);
+
+  const std::string earthPath = currentDir + "/data/earthGravity";
+  const Ballistics23::Force::EarthGravityForce earthGravity(earthPath, "egm96",
+                                                            4, 4);
+
+  const Ballistics23::Force::CelestialGravity celestialGravity;
+
+  auto forcesTuple = std::tuple(celestialGravity);
+
+  const Ballistics23::Force::AccelerationCalculator accelerationCalculator(
+      timeConverter, frameConverter, ephemerisCalculator, earthGravity,
+      forcesTuple);
+
+  const Ballistics23::TimeModule::Time<
+      Ballistics23::TimeModule::TimeScale::TT_SCALE>
+      timeTT(2458119, 0.5);
+  const Ballistics23::Vector3d position = {6800e3, 0, 0};
+  const Ballistics23::Vector3d velocity = {0, std::sqrt(398600e9 / 6800e3), 0};
+  const double mass = 1;
+
+  decltype(accelerationCalculator)::allSatParams allSatParams;
+
+  const Ballistics23::Vector3d acceleration =
+      accelerationCalculator.calcAcceleration(earthGravity, forcesTuple, timeTT,
+                                              position, velocity, mass,
+                                              allSatParams);
+
+  std::cout << acceleration;
+}
+
+TEST(ACCELERATION, GRAVITY) {
+
+  const auto begin = static_cast<Ballistics23::scalar>(0);
+  const auto end = static_cast<Ballistics23::scalar>(100000);
+
+  const std::string currentDir = getRootPath(__FILE__, 4);
+  const std::string csvPath = currentDir + "/data/frame/earth_rotation.csv";
+  const Ballistics23::indexType mjdColInd = 3;
+  const Ballistics23::indexType xpColInd = 4;
+  const Ballistics23::indexType ypColInd = 5;
+  const Ballistics23::indexType dutColInd = 6;
+
+  vector<double> mjdData = getColumn(csvPath, mjdColInd, mjdColInd, begin, end);
+  vector<double> xpData = getColumn(csvPath, xpColInd, mjdColInd, begin, end);
+  vector<double> ypData = getColumn(csvPath, ypColInd, mjdColInd, begin, end);
+  vector<double> dutData = getColumn(csvPath, dutColInd, mjdColInd, begin, end);
+
+  const Ballistics23::FrameModule::PolarMotionContainer polarMotionContainer(
+      mjdData, xpData, ypData);
+  const Ballistics23::TimeModule::DutContainer dutContainer(mjdData, dutData);
+
+  const Ballistics23::TimeModule::TimeConverter<
+      Ballistics23::TimeModule::DutContainer>
+      timeConverter(dutContainer);
+
+  const Ballistics23::FrameModule::FrameConverter<
+      Ballistics23::FrameModule::PolarMotionContainer,
+      Ballistics23::TimeModule::DutContainer>
+      frameConverter(polarMotionContainer, timeConverter);
+
+  const std::string ephemerisPath = currentDir + "/data/ephemeris/de405.bin";
+
+  const Ballistics23::Ephemeris::EphemerisCalculator ephemerisCalculator(
+      ephemerisPath);
+
+  const std::string earthPath = currentDir + "/data/earthGravity";
+  const Ballistics23::Force::EarthGravityForce earthGravity(earthPath, "egm96",
+                                                            4, 4);
+  const Ballistics23::Force::CelestialGravity celestialGravity;
+
+  auto forcesTuple = std::tuple(celestialGravity);
+
+  const Ballistics23::Force::AccelerationCalculator accelerationCalculator(
+      timeConverter, frameConverter, ephemerisCalculator, earthGravity,
+      forcesTuple);
+
+  const Ballistics23::TimeModule::Time<
+      Ballistics23::TimeModule::TimeScale::TT_SCALE>
+      timeTT(2458119, 0.5);
+  const Ballistics23::Vector3d position = {6800e3, 6800e3, 0};
+  const Ballistics23::Vector3d velocity = {0, std::sqrt(398600e9 / 6800e3), 0};
+  const double mass = 1;
+
+  decltype(accelerationCalculator)::allSatParams allSatParams;
+
+  const Ballistics23::Vector3d acceleration =
+      accelerationCalculator.calcAcceleration(earthGravity, forcesTuple, timeTT,
+                                              position, velocity, mass,
+                                              allSatParams);
+
+  std::cout << acceleration;
+}
