1e86a5592ff60fcc28e85d03cb2ccb4a1d91aa69
Tkok <murdereruu@yandex.ru>
2024-06-12 20:48:15 +0300

    + template vector

--- src/Ballistics23/utility/types/BasicTypes.h
+++ src/Ballistics23/utility/types/BasicTypes.h
@@ -20,6 +20,9 @@ using Quaternion = Eigen::Quaternion<T>;
 template <typename T>
 using Matrix3 = Eigen::Matrix<T, 3, 3>;
 
+template <typename T, unsigned int rows>
+using Vector = Eigen::Vector<T, rows>;
+
 namespace Containers {
 
 template <typename RealType>
